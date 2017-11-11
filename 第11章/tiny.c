97fc1340973511d1b65a26e7ed42f5b611ee44e4
Alan <a541203951@163.com>
2017-11-11 21:11:15 +0800

    tiny.c finished

--- 第11章/tiny.c
+++ 第11章/tiny.c
@@ -146,3 +146,51 @@ parse_uri(char *uri, char *filename, char *cgiargs)
 		return 0;
 	}
 }
+
+void serve_static(int fd, char *filename, int filesize)
+{
+	int srcfd;
+	char *srcp, filetype[MAXLINE], buf[MAXLINE];
+
+	get_filetype(filename, filetype);
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+	sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+	sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+	Rio_writen(fd, buf, strlen(buf));
+
+	srcfd = Open(filename，O_RDONLY, 0);
+	srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+	Close(srcfd);
+	Rio_writen(fd, srcp, filesize);
+	Munmap(srcp, filesize);
+}
+
+void get_filetype(char *filename, char *filetype)
+{
+	// strstr() This function returns a pointer
+	if (strstr(filename, ".html"))
+		strcpy(filetype, "text/html");
+	else if (strstr(filename, ".gif"))
+		strcpy(filetype, "image/gif");
+	else if (strstr(filename, ".jpg"))
+		strcpy(filetype, "image/jpeg");
+	else 
+		strcpy(filetype, "text/plain");
+}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Server: Tiny Web Server\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+
+	if (Fork() == 0){
+		setenv("QUERY_STRING", cgiargs, 1);
+		// Redirect stdout to client		
+		Dup2(fd, STDOUT_FILENO);
+		Execve(filename, emptylist, environ);
+	}
+	Wait(NULL);
+}
