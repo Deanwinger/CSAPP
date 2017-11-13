1684353d284a63a406026e261cc159f485d2813d
liuxiaoguang <vincexgliu@gamil.com>
2017-11-13 15:39:57 +0800

    finish echoservers.c

--- 第12章/echoservers.c
+++ 第12章/echoservers.c
@@ -76,16 +76,28 @@ void add_client(int connfd, pool *p)
 		app_error("add_client error: Too many clients");
 }
 
-// void check_client(pool *p)
-// {
-// 	int i, connfd, n;
-// 	char buf[MAXLINE];
-// 	rio_t rio;
+void check_client(pool *p)
+{
+	int i, connfd, n;
+	char buf[MAXLINE];
+	rio_t rio;
 
-// 	for (i=0; (i<= p->maxi) && (p->nready > 0); i++){
-// 		connfd = p->clientfd[i];
-// 		rio = p->clientrio[i];
+	for (i=0; (i<= p->maxi) && (p->nready > 0); i++){
+		connfd = p->clientfd[i];
+		rio = p->clientrio[i];
 
-// 		if()
-// 	}
-// }
+		if( (connfd>0) && (FD_ISSET(connfd, &p->ready_set))){
+            p->nready--;
+            if ((n=Rio_readlineb(&rio, buf, MAXLINE)) != 0){
+                byte_cnt += n
+                printf("Server received %d (%d total) bytes on fd %d\n", n, byte_cnt, connfd);
+                Rio_writen(connfd, buf, n)
+            }
+            else{
+                Close(connfd);
+                FD_CLR(connfd, &p->read_set);
+                p->clientfd[i] = -1;
+            }
+        }
+	}
+}
